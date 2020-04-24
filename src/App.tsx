9b38d07a3e404fb5475af73f82e689bd1230ceb1
Jobe Michael <jobumichael@gmail.com>
2020-04-24 22:39:18 +0200

    Add react router dom and simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Details from "./routes/Details";
+import Home from "./routes/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/details/:year" component={Details} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
