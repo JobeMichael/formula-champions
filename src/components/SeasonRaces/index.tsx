4c6c44ef6658eb98337752087646cb0ac359932e
Jobe Michael <jobumichael@gmail.com>
2020-04-25 17:54:37 +0200

    Fix console error - by changing key value in details page

--- src/components/SeasonRaces/index.tsx
+++ src/components/SeasonRaces/index.tsx
@@ -1,33 +1,29 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 
-const SeasonRaces = ({ data }: any) => {
-  console.log(data);
-  return (
-    <Table responsive>
-      <thead>
-        <tr>
-          <th>Race</th>
-          <th>Driver</th>
-          <th>Team</th>
-          <th>Nationality</th>
-          <th>Time</th>
+const SeasonRaces = ({ data }: any) => (
+  <Table responsive striped bordered hover>
+    <thead>
+      <tr>
+        <th>Race</th>
+        <th>Driver</th>
+        <th>Team</th>
+        <th>Nationality</th>
+        <th>Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      {data.map(({ name, nationality, team, season, time, raceName }: any) => (
+        <tr key={raceName}>
+          <td>{raceName}</td>
+          <td>{name}</td>
+          <td>{team}</td>
+          <td>{nationality}</td>
+          <td>{time}</td>
         </tr>
-      </thead>
-      <tbody>
-        {data.map(
-          ({ name, nationality, team, season, time, raceName }: any) => (
-            <tr key={season}>
-              <td>{raceName}</td>
-              <td>{name}</td>
-              <td>{team}</td>
-              <td>{nationality}</td>
-              <td>{time}</td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </Table>
-  );
-};
+      ))}
+    </tbody>
+  </Table>
+);
+
 export default SeasonRaces;
