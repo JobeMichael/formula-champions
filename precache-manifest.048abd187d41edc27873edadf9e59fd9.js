378c953798f370eacb70bae08107f0296d11233f
Jobe Michael <jobumichael@gmail.com>
2020-04-27 09:52:04 +0200

    Updates

---
+++ precache-manifest.048abd187d41edc27873edadf9e59fd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3335516c0b32adfefe5c1aee3c69c875",
+    "url": "/JobeMichael/formula-champions/index.html"
+  },
+  {
+    "revision": "455443c5d810e581059c",
+    "url": "/JobeMichael/formula-champions/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e4f2e08fbabe9561be90",
+    "url": "/JobeMichael/formula-champions/static/css/main.4de5db39.chunk.css"
+  },
+  {
+    "revision": "455443c5d810e581059c",
+    "url": "/JobeMichael/formula-champions/static/js/2.28b378b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/JobeMichael/formula-champions/static/js/2.28b378b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f2e08fbabe9561be90",
+    "url": "/JobeMichael/formula-champions/static/js/main.bffd3c5c.chunk.js"
+  },
+  {
+    "revision": "ee68907e68837ca061f0",
+    "url": "/JobeMichael/formula-champions/static/js/runtime-main.3570be8b.js"
+  }
+]);
